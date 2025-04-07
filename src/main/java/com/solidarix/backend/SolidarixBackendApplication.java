948bc6b95e4cdc9e01c75ed4dfec0fe03c1ee7c7
Nans FANO <data1.chau@chaussende.com>
2025-04-07 21:05:42 +0200

    Initialisation du porjet avec Maven

---
+++ src/main/java/com/solidarix/backend/SolidarixBackendApplication.java
@@ -0,0 +1,13 @@
+package com.solidarix.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidarixBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidarixBackendApplication.class, args);
+	}
+
+}
